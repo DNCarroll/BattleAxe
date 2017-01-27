8b047c30dc509a23e98a41b91f3da5925fbde3ea
Nathan Carroll <ncarroll@boongroup.com>
2017-01-27 13:40:27 -0600

    Version update.

--- BattleAxe/Properties/AssemblyInfo.cs
+++ BattleAxe/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("16.11.03.12")]
-[assembly: AssemblyFileVersion("16.11.03.12")]
+[assembly: AssemblyVersion("17.01.27.01")]
+[assembly: AssemblyFileVersion("17.01.27.01")]
